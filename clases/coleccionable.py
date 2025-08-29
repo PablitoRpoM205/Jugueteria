9753b8b7f84c5199353b36d6d5565d340be162e3
PablitoRpoM205 <juamparemu@gmail.com>
2025-08-29 17:32:52 -0500

    Feat: asignación de tipos de métodos y variables

--- clases/coleccionable.py
+++ clases/coleccionable.py
@@ -1,7 +1,7 @@
 from clases.juguete import Juguete
 
 class Coleccionable(Juguete):
-    def aplicar_descuento(self, porcentaje):
+    def aplicar_descuento(self, porcentaje: int) -> str:
         if porcentaje > 4:
             porcentaje = 4
             print("Error. El porcentaje máximo de descuento para los juguetes coleccionables es del 4%.")
--- clases/electronico.py
+++ clases/electronico.py
@@ -1,7 +1,7 @@
 from clases.juguete import Juguete
 
 class Electronico(Juguete):
-    def aplicar_descuento(self, porcentaje):
+    def aplicar_descuento(self, porcentaje: int) -> str:
         if porcentaje > 20:
             porcentaje = 20
             print("Error. El porcentaje máximo de descuento para los juguetes electrónicos es del 20%.")
